2a16685f02fca28c31cd4ee7fc4b071385766f99
Carnegie-senpai <sawyer3nick@gmail.com>
2021-10-20 18:38:39 -0700

    Fixed audio on shit-chan

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,3 +1,4 @@
+from os.path import exists
 from commands.daddy import daddy
 from commands.error import error
 from commands.help import help
@@ -5,4 +6,7 @@ from commands.insult import insult
 from commands.mock import mock
 from commands.shutdown import shutdown
 from commands.wake_on_lan import wake
-from commands.movie import movie
+if exists("./assets/training_vector_file") and exists("./assets/trained_model_output") and exists("./assets/pickled_genre"):
+    from commands.movie import movie
+else:
+    print("Movie classifier assets missing, starting without movie command enabled")
--- events/on_voice_state_update.py
+++ events/on_voice_state_update.py
@@ -6,7 +6,7 @@ from util.log_error import log_error
 
 
 @CLIENT.event
-async def on_voice_state_update(member, before, after):
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     global CURRENT_VOICE_CHANNEL
     print(member)
     prob = randrange(0, 10)
@@ -14,11 +14,11 @@ async def on_voice_state_update(member, before, after):
         try:
             soundbite = randrange(0, 11)
             CURRENT_VOICE_CHANNEL = await after.channel.connect()
-            CURRENT_VOICE_CHANNEL.play(discord.FFmpegPCMAudio("C:/Users/Nick/Desktop/DiscordBot/"+str(
-                soundbite)+".mp3", executable="C:/ffmpeg-4.1.3-win64-static/bin/ffmpeg.exe"))
+            CURRENT_VOICE_CHANNEL.play(discord.FFmpegPCMAudio(source=open("./assets/"+str(soundbite)+".mp3","rb"),pipe=True))
             while (CURRENT_VOICE_CHANNEL.is_playing()):
                 pass
             await CURRENT_VOICE_CHANNEL.disconnect()
         except Exception as e:
             log_error(e)
             print(e)
+            await CURRENT_VOICE_CHANNEL.disconnect()
